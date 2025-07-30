88a866689a8b1406db62c9502fe926a0d04dcccf
Bismillah-Enterprise <bismillah786e@gmail.com>
2025-07-31 02:57:49 +0600

    fixed error doc

--- index.js
+++ index.js
@@ -516,7 +516,7 @@ async function run() {
                         additional_movement_minute: 0
                     }
                 }
-                if (bodyData.today_exit1_time === '' && bodyData.today_exit2_time === '') {
+                if (bodyData.today_exit1_time === '' && bodyData.today_exit1_time === '' || bodyData.today_exit2_time === '') {
                     await staffsCollection.updateOne(filter, ErrorDoc, { upsert: true });
                     res.send({ message: 'Work time submitted successfully' });
                 }
