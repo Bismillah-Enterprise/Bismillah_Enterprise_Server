9339e263fe0e3ce4adc3bf5bad079be090f4289f
Bismillah-Enterprise <bismillah786e@gmail.com>
2025-08-25 18:59:24 +0600

    test local server

--- index.js
+++ index.js
@@ -12,20 +12,62 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bismillahenterpriseclus.eoxgyuj.mongodb.net/?retryWrites=true&w=majority&appName=BismillahEnterpriseCluster`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+const localUri = `mongodb://127.0.0.1:27017/Bismillah_Enterprise`
+const atlasUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bismillahenterpriseclus.eoxgyuj.mongodb.net/?retryWrites=true&w=majority&appName=BismillahEnterpriseCluster`;
+
+// Testing to connect ofline serer
+
+const createClient = (uri) =>
+    new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
+
+let client;
+
+async function connectDB() {
+    try {
+        console.log("Trying to connect to local MongoDB....");
+        client = createClient(localUri);
+        await client.connect();
+        await client.db('admin').command({ ping: 1});
+        console.log("Connected to Local MongoDB");
     }
-});
+    catch(localError) {
+        console.warn(" Local MongoDB not found. Switching to MOngoDB Atlas....");
+        try {
+            client = createClient(atlasUri);
+            await client.connect();
+            await client.db('admin').command({ping: 1});
+            console.log("Connected to MongoDB Atlas");
+        }
+        catch (atlasError) {
+            console.log("Faild to connet to both Local and Atlas MongoDB", atlasError);
+            process.exit(1);
+        }
+    }
+}
+// End Testing to connect ofline serer
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+        await connectDB();
         // Send a ping to confirm a successful connection
 
 
